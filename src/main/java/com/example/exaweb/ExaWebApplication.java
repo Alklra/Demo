8f84b1185fe4fbe74cf0c02c070d26d09c9bccd2
Alan <alanrivelo@gmail.com>
2023-09-05 13:44:22 -0600

    Se crea proyecto

---
+++ src/main/java/com/example/exaweb/ExaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.exaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exaweb/ExaWebApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.exaweb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaWebApplicationTests {
+
+
+
+}
